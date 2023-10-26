0fdf27f77fe8c9da5e57b9599e004344de145e32
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-10-26 20:11:21 +0500

    get_data

--- get_data.py
+++ get_data.py
@@ -1,5 +1,5 @@
 import json
-
+from pprint import pprint
 def get_data(file_path: str) -> dict:
     """
     get data from json file as dictionary
@@ -10,10 +10,10 @@ def get_data(file_path: str) -> dict:
     Returns:
         dict: dictionary of data
     """
-    pass
-
-
-# test
-file_path = "data.json"
+    f = open('data.json','r').read()
+    m = json.loads(f)
+    return m 
+    
+file_path = open("data.json",'r')
 data = get_data(file_path)
-print(data)
+pprint(data)
