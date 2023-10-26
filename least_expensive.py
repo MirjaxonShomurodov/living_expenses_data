f2af449439f9650dda7e7bae990ff34010e58a1c
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-10-26 20:12:30 +0500

    least_expensive

--- least_expensive.py
+++ least_expensive.py
@@ -10,9 +10,13 @@ def least_expensive(file_path: str) -> str:
     Returns:
         str: least expensive item
     """
-    pass
-
-# test
-file_path = "data.json"
+    f = open(file_path).read()
+    a = json.loads(f)
+    m = a['rent']
+    for i in a:
+        if m>a[i]:
+            m=a[i]
+    return m
+file_path = ("data.json")
 least_expensive_item = least_expensive(file_path)
 print(least_expensive_item)
